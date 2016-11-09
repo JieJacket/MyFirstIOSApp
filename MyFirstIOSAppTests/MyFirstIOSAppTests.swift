4dc8c3675f3844cb37f8958dbae0288e262fed40
wujie <jie.wu@cardinfolink.com>
2016-11-09 15:24:50 +0800

    Initial Commit

---
+++ MyFirstIOSAppTests/MyFirstIOSAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyFirstIOSAppTests.swift
+//  MyFirstIOSAppTests
+//
+//  Created by jie on 2016/11/9.
+//  Copyright © 2016年 jie. All rights reserved.
+//
+
+import XCTest
+@testable import MyFirstIOSApp
+
+class MyFirstIOSAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
